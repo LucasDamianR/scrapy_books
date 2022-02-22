ca67917ee2406c9fd133965f1c7edcaca0856eea
Lucas <lucasdamianrojas1@gmail.com>
2022-02-21 21:46:49 -0300

    update with pipes and a clean gitignore

--- book_scraper/pipelines.py
+++ book_scraper/pipelines.py
@@ -1,13 +1,21 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
-
-# useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
-
+import pymongo
 
 class BookScraperPipeline:
+
+    def __init__(self):
+        #set local connection
+        self.cnxn = pymongo.MongoClient(
+                                'localhost',
+                                27017
+                            )
+        db = self.cnxn['my_books']
+        self.collection = db['book']
+
+
     def process_item(self, item, spider):
+        #insert documents
+        self.collection.insert_one(dict(item))
         return item
